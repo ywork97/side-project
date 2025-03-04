86ac41e31e9f88543860d23cde15f742c61cfb89
ywork97 <74950269+ywork97@users.noreply.github.com>
2025-03-04 16:42:00 +0900

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -3,13 +3,6 @@ from chat_downloader import ChatDownloader
 import threading
 import time
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
-app = Flask(__name__)
-
 messages = []
 messages_lock = threading.Lock()
 downloading = False
@@ -122,5 +115,9 @@ def get_messages():
         messages.clear()
     return jsonify(new_msgs)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+app = Flask(__name__)
