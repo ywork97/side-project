5d2170f888691b9cbd1b31301f5b7acae1dffca9
ywork97 <74950269+ywork97@users.noreply.github.com>
2025-03-04 17:49:16 +0900

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -117,7 +117,5 @@ def get_messages():
         messages.clear()
     return jsonify(new_msgs)
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
