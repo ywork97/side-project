093b1c00d29c742d51d23b8e861d9491b883c2cd
ywork97 <74950269+ywork97@users.noreply.github.com>
2025-03-04 16:36:12 +0900

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,11 @@ from chat_downloader import ChatDownloader
 import threading
 import time
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 app = Flask(__name__)
 
 messages = []
